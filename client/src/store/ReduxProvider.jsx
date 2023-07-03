925a2a3215774b6bf28abf172977ee6bd5502e13
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-07-03 11:49:38 -0300

    Project structure

---
+++ client/src/store/ReduxProvider.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+/* Core */
+import { Provider } from "react-redux";
+
+/* Instruments */
+import { reduxStore } from "@/lib/redux";
+
+const ReduxProvider = ({ children }) => {
+  return <Provider store={reduxStore}>{children}</Provider>;
+};
+export default ReduxProvider;
