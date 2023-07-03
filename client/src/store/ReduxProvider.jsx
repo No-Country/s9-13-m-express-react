73d863a396af6cf9a4e4c1a94650a728a3094e50
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-07-03 19:15:51 -0300

    [client]: fix redux toolkit

--- client/src/store/ReduxProvider.jsx
+++ client/src/store/ReduxProvider.jsx
@@ -1,12 +1,12 @@
 'use client';
 
-/* Core */
 import { Provider } from 'react-redux';
+/* Core */
 
 /* Instruments */
-import { reduxStore } from '@/lib/redux';
+import { store } from './store';
 
 const ReduxProvider = ({ children }) => {
-  return <Provider store={reduxStore}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 };
 export default ReduxProvider;
