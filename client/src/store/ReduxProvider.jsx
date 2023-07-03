6e418b88230e1e013ba08b048ec6a7b630a37986
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-07-03 12:38:31 -0300

    config tailwind

--- client/src/store/ReduxProvider.jsx
+++ client/src/store/ReduxProvider.jsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 /* Instruments */
 import { reduxStore } from '@/lib/redux';
 
-let hola = ' hola';
-
 const ReduxProvider = ({ children }) => {
   return <Provider store={reduxStore}>{children}</Provider>;
 };
