86475a4f49e1d66c8c46d0443ee9a1fb0cc5eb9f
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-07-04 09:21:55 -0400

    added textField Component

---
+++ client/src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import FormComponent from "@/components/FormComponent/FormComponent";
+
+export default function LoginPage (){
+
+    return (
+    <section id="loginPage">
+        seccion de login formulario
+        <FormComponent/>
+    </section>
+    )
+}
