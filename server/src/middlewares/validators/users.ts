7ea8e05ef0955daa2c217bb634f9506bc098e29e
Leonardo Pereyra <leofranco_97@outlook.com>
2023-07-05 18:12:26 -0500

    SSB-08-Registro (#13)

--- server/src/middlewares/validators/users.ts
+++ server/src/middlewares/validators/users.ts
@@ -28,4 +28,40 @@ const validatorLogin = [
     .withMessage('Password must have more than 8 characters and less than 100 characters  '),
 ];
 
-export { validatorLogin };
+const validatorSignUp = [
+  body()
+    .custom((_value, { req }) => {
+      const allowedFields = ['email', 'password'];
+      const receivedFields = Object.keys(req.body);
+      return receivedFields.every((field) => allowedFields.includes(field));
+    })
+    .withMessage('Invalid Fields'),
+  
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username required')
+    .bail()
+    .isString()
+    .withMessage('Username is not valid'),
+
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email required')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not valid'),
+
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password required')
+    .bail()
+    .isString()
+    .withMessage('Password type is not valid')
+    .isLength({ min: 8, max: 100 })
+    .withMessage('Password must have more than 8 characters and less than 100 characters  '),
+];
+
+export { validatorLogin, validatorSignUp };
--- server/src/services/users.services.ts
+++ server/src/services/users.services.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import User from '../models/users.models';
 import { comparePassword } from '../utils/handdlePassword';
 import { jwtUtils } from '../utils/jwtUtils';
@@ -34,4 +35,18 @@ const fetchLogin = async (password: string, email: string) => {
   return response;
 };
 
-export { fetchLogin };
+const fetchSignUp = async (username: string, email: string, password: string) => {
+  const user = await findUserByEmail(email);
+  if (user) throw new Error('Email already exists!');
+  const hash = await bcrypt.hash(password, 10);
+  const data = await User.create({ username, email, password: hash });
+  // NOTA: devolvemos solo estos campos?
+  const response = {
+    id: data.id,
+    email: data.email,
+    username: data.username,
+  };
+  return response;
+};
+
+export { fetchLogin, fetchSignUp };
