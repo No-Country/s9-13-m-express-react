3fe6a4d3fd1a3aea83005c9fb82990c9b1ad612d
rossmery-garcia <66686166+rossmery-garcia@users.noreply.github.com>
2023-07-05 19:49:05 -0300

    SSB-13 Fix Login and Authentication (#12)
    - unify login and authentication

--- server/src/models/users.models.ts
+++ server/src/models/users.models.ts
@@ -1,37 +1,36 @@
-import { IUser, rolType } from 'interfaces/users.interface';
 import { model, Schema } from 'mongoose';
-
+import { IUser, rolType } from '../interfaces/users.interface';
 
 const userSchema = new Schema<IUser>({
   username: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
-    trim: true
+    trim: true,
   },
   role: {
     type: [String],
-    default: [rolType.trainee]
-  }
-})
+    default: [rolType.trainee],
+  },
+});
 
 userSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 const User = model<IUser>('User', userSchema);
 export default User;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
+import userRouter from './users.routes';
 
 const router = Router();
 
 router.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Ok' });
 });
+router.use('/auth', userRouter);
 
 export default router;
