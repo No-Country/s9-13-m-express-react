12479dcd9580890099066510895b2ee52b529144
Isaac Urdaneta <78765905+IducSoft@users.noreply.github.com>
2023-07-05 19:08:09 -0400

    Login page and form component (#8)
    * chore: install  classnames
    
    * feat: create TextField Component
    
    * added textField Component
    
    * logica interna de los formularios terminada
    
    * added textField Component
    
    * logica interna de los formularios terminada
    
    * feat: register
    
    * last changes
    
    ---------
    
    Co-authored-by: Michael Liendo <michael.m.liendo.r@gmail.com>

--- client/src/components/TextField.jsx
+++ client/src/components/TextField.jsx
@@ -18,7 +18,7 @@ export default function TextField({
     customClassNames,
     ' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
     {
-      'bg-gray-700': error,
+      'bg-gray-100': error,
       'border-red-500': error,
       'text-red-900': error,
       'placeholder-red-500': error,
