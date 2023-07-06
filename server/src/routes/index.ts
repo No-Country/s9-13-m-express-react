524338ee732c8b9faee3813555c289abe69c5656
Jose Carlos <54370505+jcarlosdf@users.noreply.github.com>
2023-07-06 10:44:37 -0500

    fix: branch recoveryPassword (#17)

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response } from 'express';
 import userRouter from './users.routes';
 import { authenticate } from '../middlewares/auth/authenticate';
 import { checkRole } from '../middlewares/auth/role';
+import recoveryPassword from './recoveryPassword.routes';
 const router = Router();
 
 router.get('/health', authenticate, checkRole(['trainee']), (req: Request, res: Response) => {
   res.status(200).json({ message: 'Ok' });
 });
 router.use('/auth', userRouter);
+router.use('/', recoveryPassword)
 
 export default router;
