8d32a5801d8cff8d8f10701d393f49f31fba7a69
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-06 16:24:22 -0400

    feat: layout auth

--- client/src/app/(auth)/register/layout.jsx
+++ client/src/app/(auth)/register/layout.jsx
@@ -1,12 +1,6 @@
 export const metadata = {
-    title:"Register",
-}
-export default function layoutRegister({children}){
-
-    return(
-        <section>
-            aqui va contenido del layout del register
-            {children}
-        </section>
-    )
+  title: 'Register',
+};
+export default function layoutRegister({ children }) {
+  return <>{children}</>;
 }
