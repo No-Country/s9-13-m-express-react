f076022d211dd11f35de84edb23141d4ae8161f3
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-07-06 21:38:22 -0400

    formularios, responsividad terminada

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -1,12 +1,84 @@
 "use client"
 
-import LoginFormComponent from "@/components/AllFormComponents/LoginFormComponent";
+import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Link from 'next/link';
+import TextField from '@/components/TextField';
+import Button from '@/components/Button';
 
-export default function LoginPage (){
 
-    return (
-    <section id="loginPage">
-        <LoginFormComponent fetchingDataFunction={(e)=>{console.log(e)}} />
-    </section>
-    )
+export default function LoginFormComponent (){
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email('Formato de correo electrónico inválido').required('Campo requerido'),
+        password: Yup.string().required('Campo requerido'),
+    });
+    
+    const formik = useFormik({
+        initialValues: {
+        email: '',
+        password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values, {resetForm}) => {
+          // Lógica para enviar el formulario
+          console.log(values);
+          fetchingDataFunction()
+          resetForm()
+          
+        },
+    });
+      
+    
+      return(
+        <div className='mx-11 2xl:mt-12'>
+          <h1 className='text-center text-4xl font-semibold 2xl:mb-12 mt-[3rem] mb-[3rem]'>
+            Iniciar Sesión.
+          </h1>
+          <form className='space-y-5 2xl:space-y-7' onSubmit={formik.handleSubmit}>
+            <TextField
+              name='email'
+              id='email'
+              label='Correo Electronico'
+              placeholder='ejemplo@ejemplo.com'
+              onChange={(value) => formik.setFieldValue('email', value)}
+              value={formik.values.email}
+              error={formik.errors.email}
+            />
+    
+            
+    
+            <TextField
+              name='password'
+              id='password'
+              type='password'
+              label='Contraseña'
+              placeholder='* * * * * * * *'
+              onChange={(value) => formik.setFieldValue('password', value)}
+              value={formik.values.password}
+              error={formik.errors.password}
+            />
+    
+            
+    
+            <Button customClassNames={"w-full bg-yellowPrimary text-purplePrimary rounded-full text-bold"} type='submit'>Iniciar Sesión.</Button>
+            <div>
+              <p className='my-2'>
+                ¿Olvidaste tu contraseña?{' '}
+              </p>
+              <p className='my-2'>
+                <span>
+                  Aun no tienes Cuenta?
+                </span>
+                <Link className='text-purple-950  text-xs  underline mx-2' href='/register'>
+                  ¡Registrate!
+                </Link>
+              </p>
+    
+            </div>
+          </form>
+        </div>
+    
+    );
 }
