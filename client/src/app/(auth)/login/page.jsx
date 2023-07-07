8a813f49f0335d17471e9a817d7a25b55422cc2d
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-07-06 20:29:46 -0400

    responsividad y estilizado para botones listo, faltan otros detalles

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -6,7 +6,6 @@ export default function LoginPage (){
 
     return (
     <section id="loginPage">
-        <h2>Iniciar Sesión</h2>
         <LoginFormComponent fetchingDataFunction={(e)=>{console.log(e)}} />
     </section>
     )
