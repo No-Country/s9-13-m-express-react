355c6e6fd0f23a26edf822904e6d1f58614d2818
adhemir sabino hilario <58167833+Adhemirsab@users.noreply.github.com>
2023-07-09 16:46:21 -0500

    SSB-15-feature-models (#23)
    -model categories and interface
    -country model and interface
    -timetable model and interface
    -model members and interface

---
+++ server/src/models/categories.models.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+import { ICategory } from '../interfaces/categories.interface';
+
+const categorySchema = new Schema<ICategory>({
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+categorySchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+const Category = model<ICategory>('Category', categorySchema);
+
+export default Category;
