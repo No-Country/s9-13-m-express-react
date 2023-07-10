308bac59f33d38c20ed339e6818be98170bfbdad
Isaac Urdaneta <78765905+IducSoft@users.noreply.github.com>
2023-07-09 23:48:11 -0400

    Feature auth and redirect (#26)
    * redux-persist configurated
    
    * redux persist configurated
    
    * problem resolved
    
    * wait

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage' 
+import persistStore from 'redux-persist/es/persistStore';
+//slices to use
+import authSlice from './slices/authSlice';
+//aqui se añade los slices que se necesiten en el proyecto.
+export const rootReducer = combineReducers({
+  //reducer
+  user: authSlice,
+  
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: persistedReducer
 });
+
+export const persistor = persistStore(store);
