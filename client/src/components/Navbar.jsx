8715087e7bc4ee60bd35e2741c788573c67fbc5a
Matias Paredes <tutecapo1985@gmail.com>
2023-07-11 16:57:22 -0300

    Optimizando diseño de navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,22 @@
 'use client';
 import { React, useState } from 'react';
 import Link from 'next/link';
-import { FaBell } from 'react-icons/fa';
+import { FaBell, FaUser } from 'react-icons/fa';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className='bg-purpleThirty shadow sticky top-0'>
+    <nav className='bg-purpleSecondary shadow sticky top-0'>
       <div className='max-w-screen-xl flex flex-row flex-wrap items-center justify-between mx-auto px-2 sm:px-4 xl:px-6 py-2'>
         <div>
           <Link href={'/'}>
-            <span className='text-2xl font-bold text-purpleSecondary hover:text-yellowPrimary hover:font-'>SKILL SWAP</span>
+            <span className='text-2xl font-bold text-yellowPrimary hover:font-bold'>
+              SKILL SWAP
+            </span>
           </Link>
         </div>
-        <div className='flex flex-row'>
+        <div className='hidden md:flex'>
           <input
             className='rounded-md shadow-lg bg-gray-100 border border-gray-300 text-purpleSecondary text-sm focus:ring-yellowPrimary block w-full pl-10 p-2.5 placeholder-purpleSecondary'
             type='search'
@@ -22,9 +24,9 @@ export default function Navbar() {
             id='search'
             placeholder=' Searching...'
           />
-          <button
+          {/* <button
             type='submit'
-            className='inline-flex shadow-lg items-center py-2.5 px-3 ml-1 text-sm font-normal text-yellowPrimary bg-purpleSecondary  hover:bg-purplePrimary hover:font-bold focus:ring-2 focus:outline-none focus:ring-yellowPrimary rounded'
+            className='inline-flex shadow-lg items-center py-2.5 px-3 ml-1 text-sm font-normal text-purpleSecondary bg-white focus:ring-2 focus:outline-none focus:ring-yellowPrimary rounded'
           >
             <svg
               aria-hidden='true'
@@ -41,15 +43,17 @@ export default function Navbar() {
                 d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
               ></path>
             </svg>
-            Search
-          </button>
+
+          </button> */}
         </div>
         <div className='hidden md:flex items-center justify-between space-x-4 '>
-          <Link href={'/login'}> <FaBell/> </Link>
-          <Link href={'/login'} className='text-purplePrimary hover:font-semibold'>PERFIL</Link>
-          {/* <Link href={'/login'}>
-            <button className='inline-flex shadow-lg items-center py-2.5 px-3 ml-1 text-sm font-normal text-yellowPrimary bg-purpleSecondary  hover:bg-purplePrimary hover:font-bold focus:ring-2 focus:outline-none focus:ring-yellowPrimary rounded'>LOGIN</button>
-          </Link> */}
+          <Link href={'/login'}>
+            {' '}
+            <FaBell className='text-yellowPrimary' />{' '}
+          </Link>
+          <Link href={'/login'}>
+            <FaUser className='text-yellowPrimary' />
+          </Link>
         </div>
         <div className='-mr-2 flex md:hidden'>
           <button
@@ -94,14 +98,51 @@ export default function Navbar() {
         </div>
       </div>
       {isOpen && (
-        <div className='md:hidden flex flex-col' id='mobile-menu'>
-          <div className='px-2 pt-2 pb-3 space-y-2 sm:px-3 flex flex-col items-center'>
-            <Link href={'/login'} className='text-purplePrimary'> <FaBell/> </Link>
-            <Link href={'/login'} className='text-purplePrimary hover:font-semibold'>PERFIL</Link>
+        <div
+          className='md:hidden flex flex-col items-center justify-center gap-5'
+          id='mobile-menu'
+        >
+          <div className='w-full flex flex-row justify-center'>
+            <input
+              className='rounded-md shadow-lg bg-gray-100 border border-gray-300 text-purpleSecondary text-sm focus:ring-yellowPrimary  pl-10 p-2.5 placeholder-purpleSecondary'
+              type='search'
+              name='search'
+              id='search'
+              placeholder=' Searching...'
+            />
+            <button
+              type='submit'
+              className='inline-flex shadow-lg items-center py-2.5 px-3 ml-1 text-sm font-normal text-yellowPrimary bg-purpleSecondary  hover:bg-purplePrimary hover:font-bold focus:ring-2 focus:outline-none focus:ring-yellowPrimary rounded'
+            >
+              <svg
+                aria-hidden='true'
+                class='mr-2 -ml-1 w-5 h-5'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                ></path>
+              </svg>
+              Search
+            </button>
+          </div>
+          <div className='px-2 pt-2 pb-3 space-y-2 sm:px-3 flex flex-col items-center gap-2'>
+            <Link href={'/login'} className='text-purplePrimary'>
+              {' '}
+              <FaBell className='text-yellowPrimary' />{' '}
+            </Link>
+            <Link href={'/login'} className='text-yellowPrimary'>
+              <FaUser />
+            </Link>
           </div>
         </div>
       )}
     </nav>
   );
 }
-
