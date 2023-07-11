48e7f48fde87028b8f3d3ba8e705494368a59db6
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-11 16:30:07 -0400

    chore: habilidades manual

---
+++ client/src/app/(app)/habilidad/page.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function Habilidades() {
+  return (
+    <div className='h-full'>
+      <h1 className='text-4xl text-center mt-5'>
+        ¡Nombre, bienvenida a SkillSwap!
+      </h1>
+      <div className='flex h-[85%] justify-center items-center'>
+        <div className='bg-yellowButtonDisable w-1/2 p-5'>
+          <h2 className='text-xl'>Seleccionaste: Otros</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
