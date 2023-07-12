8f6329421d9fc35ce115596f0c1456ce706c2319
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-12 09:50:08 -0400

    feat: habilidades

--- client/src/app/(app)/habilidad/page.jsx
+++ client/src/app/(app)/habilidad/page.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import TextField from '@/components/TextField';
+
 export default function Habilidades() {
   return (
     <div className='h-full'>
@@ -7,8 +9,42 @@ export default function Habilidades() {
         ¡Nombre, bienvenida a SkillSwap!
       </h1>
       <div className='flex h-[85%] justify-center items-center'>
-        <div className='bg-yellowButtonDisable w-1/2 p-5'>
+        <div className='bg-yellowButtonDisable w-full sm:w-1/2 p-5'>
           <h2 className='text-xl'>Seleccionaste: Otros</h2>
+          <form className='space-y-5 mt-10'>
+            <div className='flex items-center justify-between space-x-5'>
+              <label className='text-lg' htmlFor='habilidad'>
+                Habilidad
+              </label>
+              <TextField
+                name='habilidad'
+                placeholder='habilidad'
+                id={'habilidad'}
+                customClassNames='w-72'
+              />
+            </div>
+            <div className='flex items-center justify-between space-x-5'>
+              <label className='text-lg' htmlFor='descripcion'>
+                Descripción
+              </label>
+              <TextField
+                name='descripcion'
+                placeholder='descripcion'
+                id={'descripcion'}
+                customClassNames='w-72'
+              />
+            </div>
+            <div className='flex items-center justify-between space-x-5'>
+              <label className='text-lg' htmlFor='nivel'>
+                Nivel
+              </label>
+              <select className='px-5 py-4' name='nivel' id='nivel'>
+                <option value='principiante'>Principiante</option>
+                <option value='intermedio'>Intermedio</option>
+                <option value='avanzado'>Avanzado</option>
+              </select>
+            </div>
+          </form>
         </div>
       </div>
     </div>
