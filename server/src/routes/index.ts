80955af92f347d645e5c1a4f623c62be91dbaa50
jcarlosdf <josedelgado1987@gmail.com>
2023-07-14 13:24:23 -0500

    feature: Get users

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router, Response } from 'express';
-import userRouter from './auth.routes';
+import authRouter from './auth.routes';
 import recoveryPassword from './recoveryPassword.routes';
-import UserRouter from './users.routes'
+import userRouter from './users.routes'
 import memberRouter from './members.routes';
 import categoriesRouter from './categories.routes';
 import scheduleRouter from './schedule.routes';
@@ -12,11 +12,11 @@ router.get('/health', (_, res: Response) => {
   res.status(200).json({ message: 'Ok' });
 });
 
-router.use('/auth', userRouter);
+router.use('/auth', authRouter);
 router.use('/', recoveryPassword);
 router.use('/members', memberRouter);
 router.use('/categories', categoriesRouter);
 router.use('/schedule', scheduleRouter);
-router.use('/users', UserRouter);
+router.use('/users', userRouter);
 
 export default router;
