e1a78bc253c12ed199f5a8b27c722454b07f31a2
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-15 08:32:17 -0400

    fix: update user data in all request (#51)
    * fix: update user data in all request
    
    * chore: comments

--- client/src/app/(app)/layout.jsx
+++ client/src/app/(app)/layout.jsx
@@ -1,19 +1,30 @@
 'use client';
 
+// import { onLogin } from '@/store/slices/authSlice';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function AppLayout({ children }) {
   const currentUser = useSelector((state) => state.user);
   const router = useRouter();
+  //const dispatch = useDispatch();
 
   if (
     currentUser.status === 'not-authenticated' ||
     currentUser.status === 'checking'
   ) {
-
-    router.push("/login");
+    router.push('/login');
   }
+  useEffect(() => {
+    async function getUserData() {
+      const request = await getUserData(currentUser.id);
+      const response = await request.json();
+      console.log(response);
+      //dispatch(onLogin(response));
+    }
+    getUserData();
+  });
 
   return <>{children}</>;
 }
