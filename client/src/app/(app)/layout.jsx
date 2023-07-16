2abd16ee25d2ba18c7ddb1b3064a2b911992477c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-16 00:06:28 -0400

    fix:

--- client/src/app/(app)/layout.jsx
+++ client/src/app/(app)/layout.jsx
@@ -1,14 +1,11 @@
 'use client';
 
-// import { onLogin } from '@/store/slices/authSlice';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function AppLayout({ children }) {
   const currentUser = useSelector((state) => state.user);
   const router = useRouter();
-  //const dispatch = useDispatch();
 
   if (
     currentUser.status === 'not-authenticated' ||
@@ -16,15 +13,6 @@ export default function AppLayout({ children }) {
   ) {
     router.push('/login');
   }
-  useEffect(() => {
-    async function getUserData() {
-      const request = await getUserData(currentUser.id);
-      const response = await request.json();
-      console.log(response);
-      //dispatch(onLogin(response));
-    }
-    getUserData();
-  }, []);
 
   return <>{children}</>;
 }
