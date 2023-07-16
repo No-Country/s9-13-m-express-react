f3c44599b0bb9be71c33224cb0d6602a2018fc36
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-16 10:48:48 -0400

    feat: navbar for unauthenticated users

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,10 @@
 import { React, useState } from 'react';
 import Link from 'next/link';
 import { FaBell, FaUser, FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Navbar() {
+  const currentUser = useSelector((state) => state.user);
   const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -25,72 +27,86 @@ export default function Navbar() {
             </span>
           </Link>
         </div>
-        <div className='hidden md:flex items-center'>
-          <input
-            type='text'
-            value={searchTerm}
-            onChange={handleInputChange}
-            placeholder='¿Que buscas aprender?...'
-            className='py-2 px-4 rounded-l-md focus:outline-none text-purpleSecondary pl-10 p-2.5 placeholder-gray-100 w-full'
-          />
-          <button
-            onClick={handleSearch}
-            className='bg-white text-black py-2 px-4 rounded-r-md h-10'
-          >
-            <FaSearch />
-          </button>
-        </div>
-
-        <div className='hidden md:flex items-center justify-between space-x-4 '>
-          <Link href={'/login'}>
-            {' '}
-            <FaBell className='text-yellowPrimary' />{' '}
-          </Link>
-          <Link href={'/login'}>
-            <FaUser className='text-yellowPrimary' />
-          </Link>
-        </div>
-        <div className='-mr-2 flex md:hidden'>
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            type='button'
-            className='bg-yellowPrimary inline-flex items-center justify-center p-2 rounded-md text-purpleSecondary hover:text-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-gray-800 focus:ring-yellowPrimary'
-            aria-controls='mobile-menu'
-            aria-expanded={isOpen}
-          >
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              className={`block h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              aria-hidden='true'
+        {currentUser.status == 'authenticated' && (
+          <div className={'hidden md:flex items-center'}>
+            <input
+              type='text'
+              value={searchTerm}
+              onChange={handleInputChange}
+              placeholder='¿Que buscas aprender?...'
+              className='py-2 px-4 rounded-l-md focus:outline-none text-purpleSecondary pl-10 p-2.5 placeholder-gray-100 w-full'
+            />
+            <button
+              onClick={handleSearch}
+              className='bg-white text-black py-2 px-4 rounded-r-md h-10'
             >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h16M4 18h16'
-              />
-            </svg>
-            <svg
-              className={`block h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M6 18L18 6M6 6l12 12'
-              />
-            </svg>
-          </button>
-        </div>
+              <FaSearch />
+            </button>
+          </div>
+        )}
+        {currentUser.status === 'authenticated' ? (
+          <>
+            <div className='hidden md:flex items-center justify-between space-x-4 '>
+              <Link href={'/home'}>
+                {' '}
+                <FaBell className='text-yellowPrimary' />{' '}
+              </Link>
+              <Link href={'/home'}>
+                <FaUser className='text-yellowPrimary' />
+              </Link>
+            </div>
+            <div className='-mr-2 flex md:hidden'>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type='button'
+                className='bg-yellowPrimary inline-flex items-center justify-center p-2 rounded-md text-purpleSecondary hover:text-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-gray-800 focus:ring-yellowPrimary'
+                aria-controls='mobile-menu'
+                aria-expanded={isOpen}
+              >
+                <span className='sr-only'>Open main menu</span>
+                <svg
+                  className={`block h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  aria-hidden='true'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16M4 18h16'
+                  />
+                </svg>
+                <svg
+                  className={`block h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  aria-hidden='true'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M6 18L18 6M6 6l12 12'
+                  />
+                </svg>
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className='space-x-7'>
+            <Link className='text-white' href={'/login'}>
+              Login
+            </Link>
+            <Link className='text-white' href={'/register'}>
+              Registro
+            </Link>
+          </div>
+        )}
       </div>
       {isOpen && (
         <div
