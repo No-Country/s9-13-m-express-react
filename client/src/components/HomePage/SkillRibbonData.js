b58d12479bd9c62310efd88790d16bdb2a9b747f
Matias Javier Paredes <tutecapo1985@gmail.com>
2023-07-17 00:55:46 -0300

    Feat/creating home page (#54)
    * initial commit
    
    * Creation home view and components
    
    * Making changes on feature
    
    * Adding to HP sidebar and ribbons components
    
    * Uploading homePage feature

---
+++ client/src/components/HomePage/SkillRibbonData.js
@@ -0,0 +1,26 @@
+export const skillsData = [
+    {
+      title: 'Idiomas',
+      link: '/',
+    },
+    {
+      title: 'Tecnologia',
+      link: '/',
+    },
+    {
+      title: 'Cocina',
+      link: '/',
+    },
+    {
+      title: 'Artes',
+      link: '/',
+    },
+    {
+      title: 'Yoga',
+      link: '/',
+    },
+    {
+      title: 'Ver Todas',
+      link: '/',
+    },
+  ];
