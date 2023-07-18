fcdaf3f93a94e7ff822b9cf2abae09db64cb9c3e
Matias Paredes <tutecapo1985@gmail.com>
2023-07-17 23:25:12 -0300

    Uploading footer, adding Montserrat font, logo img

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
+import { koulen, montserrat } from '../app/styles/fonts';
 
 export default function Footer() {
   return (
     <footer className='bg-purpleSecondary shadow'>
-      <div className='max-w-screen-xl flex flex-row flex-wrap items-center justify-between mx-auto py-4 px-4 text-white'>
-        <div className='mx-auto'>
-          <h2 className='text-1xl font-bold '>2023 - SkillSwap.com - All rights reserved.</h2>
+      <div className='max-w-full flex flex-col items-center gap-6 py-10 text-yellowPrimary font-semibold md:flex-row md:justify-between md:px-16'>
+        <div className='flex flex-row items-center'>
+          <Image src={'/images/Logo.svg'} width={50} height={47} alt='Logo' />
+          <span className={`${koulen.className} text-3xl ml-1`}>
+            SKILL SWAP
+          </span>
+        </div>
+        <div>
+          <p className={`${montserrat.className}`}>
+            Todos los derechos reservados ©
+          </p>
+        </div>
+        <div>
+          <p className={`${montserrat.className}`}>2023</p>
         </div>
       </div>
     </footer>
   );
 }
-
-
