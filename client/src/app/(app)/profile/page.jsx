eabd4a5c7f626d00720a02e5779017e5e3246f8a
Matias Javier Paredes <tutecapo1985@gmail.com>
2023-07-20 02:02:57 -0300

    Feat/user profile (#68)
    * Starting layout and profile components
    
    * fixing logo
    
    * Uploading last changes on feature

---
+++ client/src/app/(app)/profile/page.jsx
@@ -0,0 +1,24 @@
+import SideBar from '@/components/HomePage/SideBar';
+import UserProfileCard from '@/components/UserProfile/UserProfileCard';
+import Link from 'next/link';
+import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+
+export default function userProfile() {
+  return (
+    <main className='min-h-screen flex flex-row bg-white font-semibold'>
+      <div className='bg-purpleThirty mx-auto flex-shrink-0 pt-10 shadow-xl'>
+        <SideBar show={'profile'} />
+      </div>
+      <div className='mx-auto flex-grow pt-10 px-10 md:px-12 lg:px-16'>
+        <div className='flex flex-row items-center justify-start mb-4'>
+          <Link href={'/'} className='text-lg mb-6'>
+            <FaArrowLeft className='inline-flex mr-1' />
+            <span>Atras</span>
+          </Link>
+        </div>
+        <UserProfileCard />
+      </div>
+    </main>
+  );
+}
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 import { koulen, montserrat } from '../app/styles/fonts';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className='bg-purpleSecondary shadow'>
       <div className='max-w-full flex flex-col items-center gap-6 py-10 text-yellowPrimary font-semibold md:flex-row md:justify-between md:px-16'>
         <div className='flex flex-row items-center'>
-          <Image src={'/images/Logo.svg'} width={50} height={47} alt='Logo' />
-          <span className={`${koulen.className} text-3xl ml-1`}>
-            SKILL SWAP
-          </span>
+          <Image
+            src='/images/logo.svg'
+            alt='skill swap logo'
+            width={50}
+            height={50}
+          />
+          <Link href={'/'}>
+            <span className={`${koulen.className} text-3xl ml-1`}>
+              SKILL SWAP
+            </span>
+          </Link>
         </div>
         <div>
           <p className={`${montserrat.className}`}>
