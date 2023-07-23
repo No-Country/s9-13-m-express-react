9a387a55da06ef9d9b58133a9f2d1bb9abd14399
Matias Paredes <tutecapo1985@gmail.com>
2023-07-22 21:34:02 -0300

    Adding edit modal comp

--- client/src/components/UserProfile/UserProfileCard.jsx
+++ client/src/components/UserProfile/UserProfileCard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+'use client';
+import { React, useState } from 'react';
 import Image from 'next/image';
 import { FaLocationDot } from 'react-icons/fa6';
 import { MdEdit } from 'react-icons/md';
+import EditModal from './EditModal';
+import Link from 'next/link';
 
-function UserProfileCard({ props }) {
-  const prop = props;
-  console.log(prop);
+function UserProfileCard() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleModal = () => {
+    setOpenModal(!openModal);
+  };
 
   return (
     <div className='bg-white grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 w-auto h-fit max-w-4xl gap-4 overflow-hidden mx-auto'>
@@ -18,6 +24,7 @@ function UserProfileCard({ props }) {
               height={120}
               alt='#'
               className='w-20 sm:w-24 md:w-32 lg:w-40 rounded-full'
+              // className='w-full h-full rounded-full'
             />
           </div>
           <div className='flex flex-col content-between gap-4'>
@@ -35,9 +42,19 @@ function UserProfileCard({ props }) {
               </span>
             </h4>
           </div>
-          <div className='flex flex-col'>
-            <MdEdit className='text-purpleSecondary' />
-            <FaLocationDot className='text-purpleSecondary' />
+          <div className='flex flex-col gap-16'>
+            <div className='flex justify-end'>
+              <Link href={'#'} className=''>
+                <MdEdit
+                  className='text-purpleSecondary'
+                  onClick={handleModal}
+                />
+              </Link>
+            </div>
+            <div className='flex flex-row items-center gap-1'>
+              <FaLocationDot className='text-purpleSecondary inline' />
+              <span>California, USA</span>
+            </div>
           </div>
         </div>
       </div>
@@ -57,9 +74,9 @@ function UserProfileCard({ props }) {
         <div className='flex flex-col gap-8'>
           <div className='flex flex-row items-center justify-center gap-4 px-4'>
             <h2 className='text-xl'>Disponibilidad</h2>
-            <span>
-              <MdEdit />
-            </span>
+            <Link href={'#'}>
+              <MdEdit onClick={handleModal} className='text-purpleSecondary' />
+            </Link>
           </div>
           <div className='flex flex-col gap-4'>
             <p>Dias: </p>
@@ -93,6 +110,7 @@ function UserProfileCard({ props }) {
           </div>
         </div>
       </div>
+      {openModal && <EditModal modalState={handleModal} />}
     </div>
   );
 }
