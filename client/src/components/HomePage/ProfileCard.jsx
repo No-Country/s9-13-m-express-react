58c45cd242c7e5621592418386230395684e55f9
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-25 12:07:29 -0400

    fix

--- client/src/components/HomePage/ProfileCard.jsx
+++ client/src/components/HomePage/ProfileCard.jsx
@@ -44,7 +44,11 @@ export default function ProfileCard({ profileData, isSingle }) {
               </div>
               <div className='flex flex-row gap-10 items-center justify-between'>
                 <h5 className='text-lg'>Valoraciones: ⭐⭐⭐⭐⭐</h5>
-                <Link href={`/profile/${profileData.id}`}>
+                <Link
+                  href={`/profile/${
+                    profileData.id || profileData._id || profileData.user
+                  }`}
+                >
                   <button className='bg-yellowPrimary px-4 py-2 mx-auto text-purplePrimary rounded-3xl w-full'>
                     Ver perfil
                   </button>
