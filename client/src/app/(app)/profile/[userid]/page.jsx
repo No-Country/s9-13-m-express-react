fd43a70374722878e25272b9a4da65a87c9f8c6b
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-25 12:05:52 -0400

    feat: dinamic route of user

--- client/src/app/(app)/profile/[userid]/page.jsx
+++ client/src/app/(app)/profile/[userid]/page.jsx
@@ -16,7 +16,7 @@ export default function Page({ params }) {
         `https://skillswap.onrender.com/api/v1/users/${userid}`
       );
       const response = await request.json();
-      console.log(response);
+      console.log(response?.user?.at(0));
       setUser(response?.user?.at(0));
     }
 
